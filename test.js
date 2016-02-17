030c9b30a542970c250722ed99444ccdf25072c5
David Björklund <david.bjorklund@gmail.com>
2016-02-17 07:35:55 -0500

    youtube

--- test.js
+++ test.js
@@ -25,3 +25,27 @@ test('twitter', t => {
 
   t.end();
 });
+
+test('youtube', t => {
+  t.plan(2);
+
+  const Renderer = setupRenderer({
+    embeds: {
+      youtube: embed => {
+        t.equal(embed.id, 'youtube-id');
+        return <span id={embed.id} />;
+      }
+    }
+  });
+
+  const data = [{
+    type: 'embed',
+    embedType: 'youtube',
+    id: 'youtube-id'
+  }];
+  const expected = string.render(<article><span id='youtube-id'/></article>);
+  const actual = string.render(<Renderer data={data} />);
+
+  t.equal(actual, expected);
+  t.end();
+});
