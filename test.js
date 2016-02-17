38c4c7404827d74879856c77f9665d5773ab9b9c
David Björklund <david.bjorklund@gmail.com>
2016-02-17 07:27:35 -0500

    handle twitter

---
+++ test.js
@@ -0,0 +1,27 @@
+import test from 'tape';
+import setupRenderer from './lib';
+import {element, string} from 'deku';
+
+test('twitter', t => {
+  t.plan(2);
+
+  const Renderer = setupRenderer({
+    embeds: {
+      twitter: tweet => {
+        t.equal(tweet.id, 'twitter-id');
+        return <span id={tweet.id} />;
+      }
+    }
+  });
+  const data = [{
+    type: 'embed',
+    embedType: 'twitter',
+    id: 'twitter-id'
+  }];
+  const expected = string.render(<article><span id='twitter-id'/></article>);
+  const actual = string.render(<Renderer data={data} />);
+
+  t.equal(actual, expected);
+
+  t.end();
+});
