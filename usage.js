7aca8ea69fde435050038cf70757a91caac34882
Serge <iefserge@runtimejs.org>
2017-02-08 02:51:37 +0200

    Revert "Add ability to render html directly"

--- usage.js
+++ usage.js
@@ -1,21 +1,15 @@
 import setupArticle from 'article-json-html-render';
-import setupArticleToHtml from 'article-json-html-render/html';
 import {element, string} from 'deku';
 
-const items = [
-  {type: 'paragraph', children: [{content: 'foo'}]},
-  {type: 'embed', embedType: 'image', src: 'http://example.com/image.jpg'}
-];
-
-// Render into Deku component tree
 const Article = setupArticle({
   embeds: {
     image: embed => <img src={embed.src} />
   }
 });
 
-console.log(string.render(<Article items={items} />));
+const items = [
+  {type: 'paragraph', children: [{content: 'foo'}]},
+  {type: 'embed', embedType: 'image', src: 'http://example.com/image.jpg'}
+];
 
-// Render into html
-const renderHtml = setupArticleToHtml();
-console.log(renderHtml(items));
+console.log(string.render(<Article items={items} />));
